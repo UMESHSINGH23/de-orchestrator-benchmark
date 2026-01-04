1f9d518a87bdd9702cc33bb94d29266ada709928
Umesh Singh <parihar.umesh@gmail.com>
2026-01-04 14:52:28 +0100

    Improve model training logic and logging

--- src/model_training/train_model.py
+++ src/model_training/train_model.py
@@ -1,1 +1,9 @@
+from sklearn.linear_model import LinearRegression
 
+def train_model(X, y):
+    """
+    Train a simple interpretable model.
+    """
+    model = LinearRegression()
+    model.fit(X, y)
+    return model
